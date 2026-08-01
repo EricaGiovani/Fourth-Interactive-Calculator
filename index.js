0f57fa588723c4fa34742b2f0d8e90cbc9998106
Erica Giovani <ericagiovani99@gmail.com>
2026-08-01 15:12:48 +0700

    create a getNumber

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ console.log("===Interactive Calculator===");
 const name = prompt('Masukkan Nama: ');
 console.log(`Welcome to Interactive Calculator, ${name}!`)
 
+function getNumber(){
+  while(true){
+    const input = prompt('Enter the Number: ');
+    const number = Number(input);
+  
+  if (isNaN(number)){
+    console.log('Must be a Number!');
+  } else{
+    return number;
+  }
+}
+}
+getNumber();
