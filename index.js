b3232483529d92267918c54252bc0c5118e6340e
Erica Giovani <ericagiovani99@gmail.com>
2026-08-01 14:56:19 +0700

    creating the initial calculator display

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code here, goodluck!!
+'use strict';
+const prompt = require("prompt-sync")();
+
+console.log("===Interactive Calculator===");
+const name = prompt('Masukkan Nama: ');
+console.log(`Welcome to Interactive Calculator, ${name}!`)
+
